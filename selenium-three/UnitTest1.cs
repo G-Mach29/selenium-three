c0ccac8245754f3c14da9a943496b089fcfb3163
gmacherla <gmacherla@gmail.com>
2024-08-05 05:24:47 +0100

    Add project files.

---
+++ selenium-three/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace selenium_three
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
